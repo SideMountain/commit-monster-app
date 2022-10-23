790d338d94aded3d65fe8d3ce5b4cf2d7d9f0d3e
yuya.yokoyama <yuya.yokoyama@monocrea.co.jp>
2022-10-23 22:49:33 +0900

    mod 4

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Hello Commit Monster3</div>
+  <div>Hello Commit Monster4</div>
 </template>
 
 <script lang="ts">
--- push7-worker.js
+++ push7-worker.js
@@ -1,1 +1,2 @@
-importScripts("https://aldebaran.push7.jp/ex-push7-worker.js");
+importScripts('https://aldebaran.push7.jp/ex-push7-worker.js')
+importScripts('sw.js')
